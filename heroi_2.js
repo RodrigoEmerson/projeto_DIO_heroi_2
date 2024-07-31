70f2db17fabfa54cd5deb2344490b10e047c251b
RodrigoEmerson <127218838+RodrigoEmerson@users.noreply.github.com>
2024-07-31 17:04:48 -0300

    Create heroi_2.js

---
+++ heroi_2.js
@@ -0,0 +1,38 @@
+
+
+def ranqueada(vitoria, derrota):
+    rank = vitoria - derrota
+    return rank
+
+
+def nivel():
+    if ranqueada(quantidadeVitoria, quantidadeDerrota) <= 10:
+        return "FERRO"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) <= 20:
+        return "BRONZE"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) <= 50:
+        return "PRATA"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) <= 80:
+        return "OURO"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) <= 90:
+        return "DIAMANTE"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) <= 100:
+        return "LENDÁRIO"
+    elif ranqueada(quantidadeVitoria, quantidadeDerrota) >= 101:
+        return "IMORTAL"
+    else:
+        print("ERRO INEXPERADO")
+
+
+quantidadeVitoria = int(input("Digite a quantidade de vitoria "))
+quantidadeDerrota = int(input("Digite a quantidade de derrota "))
+
+
+print(f"""
+O Herói tem 
+{quantidadeVitoria} de vitorias
+{quantidadeDerrota} de derrotas
+
+tem saldo de vitorias de {ranqueada(quantidadeVitoria, quantidadeDerrota)} 
+esta no nivel {nivel()}!!! 
+""")
